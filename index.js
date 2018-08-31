133979bb0bc1f0a20137c571d557cb1bdaef0451
thejosea@gmail.com <thejosea@gmail.com>
2018-08-31 02:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello, Everybody";
+var greeting = "Hello, Everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Neil deGrasse Tyson" + specialGuest + "Hello, Everybody!";
 
 var topic = "space";
 
